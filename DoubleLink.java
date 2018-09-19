335ef1c9b09d2580c7d2e0ca4b6471f9e7f03a48
zzj <38907165+zzjzzy@users.noreply.github.com>
2018-09-19 11:16:04 +0800

    Update DoubleLink.java

--- DoubleLink.java
+++ DoubleLink.java
@@ -8,6 +8,39 @@ class DoubleLink<T> {
     public T value; //节点值
     public Node<T> prev; //节点的前一个节点
     public Node<T> next; //节点的后一个节点
+    
+    //构造函数初始化节点
+    public Node(T value, Node<T> prev, Node<T> next){
+      this.value = value;
+      this.prev = prev;
+      this.next = next;
+    }
+  }
+  
+  //初始化链表
+  public DoubleLink(){
+    head = new Node(null, null, null);
+    head.prev = head.next = head;
+  }
+  
+  //获得指定index的节点，外部一般获取节点值，获取节点没意义，所以定义为private
+  private Node<T> getNode(int index){
+    //如果index在前半部分，就用前序遍历
+    if(index <= count/2){
+      Node<T> node = head.next;
+      for(int i = 0; i < index; i++){
+        node = node.next;
+      }
+      return node;
+    }
+    
+    //如果index在后半部分，就用后序遍历
+    Node<T> node = head.prev;
+    rindex = count - 1 - index;
+    for(int i = 0; i < rindex; i++){
+      node = node.prev;
+    }
+    return node;
   }
 
 
